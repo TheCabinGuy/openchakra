0dc229f07a600b1b97a36a14125129c79140ec3c
Pierre <pierre.crbs@gmail.com>
2020-02-13 10:22:36 +0100

    rename const & fix focus

--- src/hooks/useInteractive.ts
+++ src/hooks/useInteractive.ts
@@ -31,7 +31,6 @@ export const useInteractive = (
       event.preventDefault()
       event.stopPropagation()
       dispatch.components.select(component.id)
-      dispatch.app.toggleInputText()
     },
     onDoubleClick: (event: MouseEvent) => {
       event.preventDefault()
